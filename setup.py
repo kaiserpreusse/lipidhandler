d2c186da04fe024aa1dcd5b74ad60d7e159e819b
Martin <martin.preusse@gmail.com>
2020-11-06 18:29:44 +0100

    bump to 0.0.6

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
+from os import path
+
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(name='lipidhandler',
-      version='0.0.5',
+      version='0.0.6',
       description='Handle lipid names in Python.',
+      long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/kaiserpreusse/lipidhandler',
       author='Martin Preusse',
