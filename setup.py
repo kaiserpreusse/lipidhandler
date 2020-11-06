52749cf0d72374c10803789b5d32d6069edc1b53
Martin <martin.preusse@gmail.com>
2020-11-06 14:48:45 +0100

    bump to 0.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='lipidhandler',
-      version='0.0.4',
+      version='0.0.5',
       description='Handle lipid names in Python.',
       long_description_content_type='text/markdown',
       url='https://github.com/kaiserpreusse/lipidhandler',
